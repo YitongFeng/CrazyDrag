347f5af377b6cc4e8d9c5844cbbfbe2a0fad6996
fyt <imfengyitong@hotmail.com>
2016-03-03 18:08:03 +0800

    UI set up complete

---
+++ CrazyDrag/CrazyDrag/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrazyDrag
+//
+//  Created by YitongFeng on 3/3/16.
+//  Copyright (c) 2016 YitongFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrazyDrag/CrazyDrag/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrazyDrag
+//
+//  Created by YitongFeng on 3/3/16.
+//  Copyright (c) 2016 YitongFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
